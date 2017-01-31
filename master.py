f01f8a9abc78833bf4db43c3b0c432a3c73f0aa7
Marcus Munch <marcus@marcusmunch.dk>
2017-01-31 18:04:58 +0100

    0.1
    Added: A script "master.py" that executes all .py scripts in ./scripts
    and outputs logs and output files accordingly.

---
+++ master.py
@@ -0,0 +1,41 @@
+#import modules
+import os, subprocess, sys, inspect
+from time import localtime, strftime
+
+#define variables
+def outputWrite(currentName, data):
+	if not os.path.exists('../output'):
+		os.makedirs('../output')
+	outputFile = open('../output/' + currentName + '.txt', 'w')
+	print (currentName + ' says: ' + strftime('[%X %x]: ' + data))
+	outputFile.write(data)
+	outputFile.close()
+	logWrite(currentName, data)
+
+def logWrite(outputWrite, data):
+	if not os.path.exists('../logs'):
+		os.makedirs('../logs')
+	logFile = open('../logs/' + currentName + '.log', 'a')
+	logFile.write(strftime('\n[%x %X]: Wrote to file ' + currentName + '.txt: ' + (data)))
+	logFile.close()	
+
+def execAll():
+	global currentName
+	os.chdir(r'scripts')
+	print "Executing all scripts in '/scripts'..."
+	for i in os.listdir('.'):
+		currentName = (i)[:-3]
+		execScr(i)
+
+def execScr(f):
+	if f[-3:] == ".py":
+		execfile(f)
+	else:
+		print "An error has occurred!"
+
+#execute script		
+def main():
+	execAll()
+	
+if __name__ == "__main__":
+        main()
---
+++ scripts/helloworld.py
@@ -0,0 +1,2 @@
+data = "Hello world!"
+outputWrite(currentName, data)
