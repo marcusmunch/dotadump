c052be9370a8c3abc24ba72d033086943a3e9e1d
Marcus Munch <marcus@marcusmunch.dk>
2017-04-01 13:24:49 +0200

    Renamed variables and added config.ini loader
    Known bugs:
    
    - Skipping input prompts deletes config.ini

--- setup.py
+++ setup.py
@@ -1,36 +1,48 @@
 #!/usr/bin/python
 import os, json
 
-def compileOutput():
-    global output
+def addGlobals():
+    global output, oldConfig, FTPaddr, FTPpass, steamID
     output = {}
+
+def loadOldConfig():
+    if os.path.exists("config.ini"):
+        global oldConfig
+        file = open("config.ini").read()
+        oldConfig = json.loads(file)
+    else:
+        file = open("config.ini", "w")
+        file.write("{}")
+
+def compileOutput():
     if 'FTPaddr' in globals():
-        output['FTP_address'] = FTPaddr
-        output['FTP_password'] = FTPpass
+        output['FTPaddr'] = FTPaddr
+        output['FTPaddr'] = FTPpass
+        print 'FTP logon credentials updated'
     if 'steamID' in globals():
-        output['steam32id'] = steamID
+        output['steamID'] = steamID
+        print 'Steam32-ID updated'
     if output != {}:
         file = open("config.ini", 'w')
         file.write(json.dumps(output))
         file.close()
-    else:
-        if os.path.isfile('config.ini'):
-            os.remove("config.ini")
-            print "\nConfig.ini deleted"
-        else:
-            print "\nConfig.ini not existing and not created."
     
 def promptFTP():
     while True:
         resp = raw_input ("Update FTP address? Y/N ").lower()
         if resp.lower() == 'n':
             print "Skipping!"
+            FTPaddr = oldConfig['FTPaddr']
+            FTPpass = oldConfig['FTPpass']
             break
         elif resp != 'y' and resp != 'n': print 'Input not accepted, try again'
         elif resp == 'y': pass
-        global FTPaddr, FTPpass
-        FTPaddr = raw_input('Enter FTP address: ')
-        FTPpass = raw_input('Enter FTP password: ')
+        input = raw_input('Enter FTP address: ')
+        if input != '': FTPaddr = input
+        else: FTPaddr = oldConfig['FTPaddr']
+        input = raw_input('Enter FTP password: ')
+        if input != '': FTPpass = input
+        else: FTPpass = oldConfig['FTPaddr']
         break
 
 def promptID():
@@ -42,13 +54,16 @@ def promptID():
         elif resp == "y": pass
         else: print "Input not accepted, try again"
         resp2 = raw_input ("Enter Steam32 ID: ")
-        if resp2.isdigit():
-            global steamID
+        if resp2 == '':
+            steamID = oldConfig['steamID']
+        elif resp2.isdigit():
             steamID = resp2
             break
         else: print "Input is not all digits. Try again"
 
 if __name__ == "__main__":
+    addGlobals()
+    loadOldConfig()
     promptFTP()
     promptID()
     compileOutput()
