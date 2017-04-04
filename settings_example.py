de3e90dbe56ad9c57d628a77cd566f8db58d2d0c
marcusmunch <marcus@marcusmunch.dk>
2017-04-04 17:08:27 +0200

    Create settings_example.py

---
+++ settings_example.py
@@ -0,0 +1,7 @@
+STEAM_ID = '3213567' # Your Steam32 ID
+FTP_ADDR = 'ftp.domain.com' # The address for your FTP connection. This will also be used as the username to login.
+FTP_PASS = 'hunter2' # The password for your FTP connection.
+
+SUGGEST_MIN_DAYS = 30 # Minimum days since you played the heroes picked for HeroSuggester.py
+SUGGEST_MIN_GAMES = 10 # Minimum number of games played on heroes picked for HeroSuggester.py
+SUGGEST_MIN_WINRATE = 50 # Minimum winrate in %. NOTE: NOT IMPLEMENTED YET
