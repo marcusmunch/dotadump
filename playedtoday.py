95f1a062032e086152290ff0a1ea0ce237077758
marcusmunch <marcus@marcusmunch.dk>
2017-06-16 21:56:51 +0200

    Enabled further debug options
    When debugging, script will now fetch matches for the number of days set in line 22.
    
    Also did small corrections and formatting.

--- playedtoday.py
+++ playedtoday.py
@@ -17,36 +17,46 @@ def setLimit():
 	reset_time = time.mktime((time.localtime()[0], time.localtime()[1], time.localtime()[2], 4, 0, 0, -1, -1, -1))
 	reset_struct = time.localtime(reset_time)
 
+
 def matchesToday():
-	r = requests.get("https://api.opendota.com/api/players/%s/matches?date=1" % settings.STEAM_ID)
+	r = requests.get("https://api.opendota.com/api/players/%s/matches?date=10" % settings.STEAM_ID)
 	data = json.loads(r.text)
 	output = {}
 	print 'Getting matches played today...'
 	for match in range(0,len(data)): # To ensure that we only get matches today and not within 24 hours
-		if 'reset_time' in globals():
+		if 'reset_time' in globals() and not settings.DEBUG_MODE:
 			if data[match]['start_time'] >= reset_time: output[match] = data[match]
 		else: output[match] = data[match]
-	identifyHeroes(output)
+	if output == {}:
+		print 'No games played today!'
+	else:
+		identifyHeroes(output)
 	return output
+
 
 def appendResult(input):
 	for i in range(0,len(input)):
-		if input[i]['player_slot'] <= 4 and input[i]['radiant_win'] == True: input[i]['result'] = 'Won'  # Radiant victory
-		elif input[i]['player_slot'] >= 128 and input[i]['radiant_win'] == False: input[i]['result'] = 'Won'  # Dire victory
-		else: input[i]['result'] = 'Lost'
+		if input[i]['player_slot'] <= 4 and input[i]['radiant_win'] == True: input[i]['result'] = 'Win'  # Radiant victory
+		elif input[i]['player_slot'] >= 128 and input[i]['radiant_win'] == False: input[i]['result'] = 'Win'  # Dire victory
+		else: input[i]['result'] = 'Loss'
 	return input
+
 
 def compileOutput():
 	HeroPool = matchesToday()
-	appendResult(HeroPool)
-	output = []
-	for item in range(0,len(HeroPool)):
-		output.append('%s (%s)' % (HeroPool[item]['localized_name'], HeroPool[item]['result']))
-	return ('Heroes played today: ' + ', '.join(output) + '.')
+	if len(HeroPool) > 0:
+		appendResult(HeroPool)
+		output = []
+		for item in range(0,len(HeroPool)):
+			output.append('%s (%s)' % (HeroPool[item]['localized_name'], HeroPool[item]['result']))
+		return ('Heroes played today: ' + ', '.join(output) + '.')
+	else: return False
+
 
 def main():
 	DotaTools.writeToFile(compileOutput(), outFile)
 	DotaTools.upload(outFile)
+
 
 if __name__ == '__main__':
 	main()
