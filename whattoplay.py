977da90126e80ae2ad10cd7354e35eb8ff8a4e0b
MarcusMunch <marcus@marcusmunch.dk>
2017-10-19 10:03:04 +0200

    Reworked whattoplay
    Now sorts hero pool by descending win rate and returns the top X heroes. This makes for more consistent output and (hopefully) more usability.
    
    Also made code a bit more easily readable and formatted a bit according
    to PEP 8.

--- whattoplay.py
+++ whattoplay.py
@@ -22,49 +22,68 @@ outFile = os.path.splitext(base)[0] + '.txt'
 def noRecent(minmatches=10, days=60):
     output = []
     print 'Finding heroes not played within the last %s days (minimum of %s games played and minimum win rate of %s%%)...' % (days, minmatches, settings.SUGGEST_MIN_WINRATE)
-    r = requests.get('https://api.opendota.com/api/players/%s/Heroes' % settings.STEAM_ID)
+    r = requests.get(
+        'https://api.opendota.com/api/players/%s/Heroes' % settings.STEAM_ID)
     data = json.loads(r.text)
     longago = time.time() - (86400 * days)
     for i in range(0, len(data)):
-        if data[i]['games'] > 0: winrate = (float(data[i]['win']) / float(data[i]['games'])) * 100
-        else: winrate = 0
-        if data[i]['last_played'] == 0: None
-        elif winrate <= settings.SUGGEST_MIN_WINRATE: None
-        elif data[i]['games'] <= minmatches: None
+        if data[i]['games'] > 0:
+            winrate = (float(data[i]['win']) / float(data[i]['games'])) * 100
+        else:
+            winrate = 0
+        if data[i]['last_played'] == 0:
+            None
+        elif winrate <= settings.SUGGEST_MIN_WINRATE:
+            None
+        elif data[i]['games'] <= minmatches:
+            None
         elif data[i]['last_played'] <= longago:
             oldHero = data[i]
             output.append(oldHero)
     return output
 
 
+def winrate(hero_dict):
+    rate = '%.2f' % (float(hero_dict['win']) / float(hero_dict['games']) * 100)
+    return ('%s%% win' % rate)
+
+
 def whatToPlay(pickFrom, suggestion_num=3):
+
     if len(pickFrom) == 0:
         print 'No heroes fulfill the selected criteria'
         return ''
+
     else:
         print '\nNow, what should you play today? \nPicking %s out of %s eligible heroes...\n' % (min(suggestion_num, len(pickFrom)), len(pickFrom))
         if suggestion_num <= 0:
             print "\nYou specifically asked for no (or less than zero(!)) suggestions!"
+
         else:
-            leader = (str(suggestion_num) + ' hero challenge for ' + time.strftime("%d/%m-%Y") + ': ')
+            leader = "Here's %s heroes you could play" % suggestion_num
             suggestions = 0
             challenge = []
-            def winrate(hero):
-                rate = '%.2f' % (float(pickFrom[hero]['win']) / float(pickFrom[hero]['games'])*100)
-                return ('(%s%% win)') % rate
+
+            for i in pickFrom:
+                i['winrate'] = winrate(i)
+            pickFrom = sorted(pickFrom, key=lambda x: x[
+                              'winrate'], reverse=True)
+
             while suggestions < suggestion_num and len(pickFrom) > 0:
-                rng = randint(0, len(pickFrom)) - 1
-                string = pickFrom[rng]['localized_name'] + ' ' + winrate(rng)
+                pick = pickFrom.pop(0)
+
+                string = '%s (%s)' % (pick['localized_name'], pick['winrate'])
                 challenge.append(string)
-                pickFrom.remove(pickFrom[rng])
                 suggestions += 1
-            output = leader + ', '.join(challenge) + '.' 
+
+            output = '%s: %s.' % (leader, ', '.join(challenge))
             return output
 
 
 def main():
     HeroPool = noRecent(settings.SUGGEST_MIN_GAMES, settings.SUGGEST_MIN_DAYS)
-    ToPlay = whatToPlay(DotaTools.identifyHeroes(HeroPool), settings.SUGGEST_AMOUNT)
+    ToPlay = whatToPlay(DotaTools.identifyHeroes(
+        HeroPool), settings.SUGGEST_AMOUNT)
     DotaTools.writeToFile(ToPlay, outFile)
     DotaTools.upload(outFile)
 
